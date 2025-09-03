39dacd10c9557d75fa1e1a5abf8fe2e644312fee
Daniel <122777459+daniel-0104@users.noreply.github.com>
2025-09-03 23:01:25 +0630

    screen ui commit

---
+++ components/SearchBar.tsx
@@ -0,0 +1,31 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+}
+
+const SearchBar = ({ onPress, placeholder }: Props) => {
+  return (
+    <View className="flex-row items-center bg-slate-900 px-5 py-2 rounded-full">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        placeholder={placeholder}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
+        value=""
+        onPress={onPress}
+        onChangeText={() => {}}
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
---
+++ services/api.ts
@@ -0,0 +1,13 @@
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTg1OGJlZTU0YTk5OGI5YWEzZmUzZmZlODA3MDU0MyIsIm5iZiI6MTc1NTUwNDEyNS41NjgsInN1YiI6IjY4YTJkZGZkOTBlMWY3NjM2ODE4ZDUwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.S5KlsMHe2eyZBlqATOHIpiYkozXJVwSRscgO7rt0MM0'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
