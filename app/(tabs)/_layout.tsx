7320439165e93f26ea05ba8f119c47dab15cadad
Daniel <122777459+daniel-0104@users.noreply.github.com>
2025-09-03 16:36:13 +0630

    second commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,46 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Text } from "@react-navigation/elements";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Image, ImageBackground } from "react-native";
+
+const _layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <ImageBackground source={images.highlight}>
+                <Image
+                  source={icons.home}
+                  tintColor="#151312"
+                  className="size-5"
+                />
+                <Text>Home</Text>
+              </ImageBackground>
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{ title: "Saved", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{ title: "Search", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{ title: "Profile", headerShown: false }}
+      />
+    </Tabs>
+  );
+};
+
+export default _layout;
